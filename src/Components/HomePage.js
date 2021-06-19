d81eead8ef2ee733db8a11675147d5f862e395ca
bayarens <bay.arens@gmail.com>
2021-06-19 12:05:31 -0400

    added style and resume page done

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -7,13 +7,13 @@ export default function Home(){
         <div id='mainContainer'>
             <img id='profilePic' src="./Profilepic.jpg"/>
             <p id='introBox'>Thank you for taking the time to come take a look at my Portfolio! My name is Bayley Arens, I am a New York City native, a Miami University Alum, and I am on my journey to become a fullstack developer. This is a website I made to house all my contact info, my resume, and to show off all my previous projects, I hope you enjoy!</p>
-        </div>
-        <div id='contactInfo'>
+            <div id='contactInfo'>
             <ul>
                 <li>Email: bay.arens@gmail.com</li>
                 <li>Phone: 646-413-1666</li>
                 <li>GitHub: https://github.com/bayarens</li> 
             </ul>
+        </div>
         </div>
     </div>
     )
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar(props){
     return(
         <div id='navBar'>
             <Link to='/'> <button className='button'>Home</button> </Link>
-            <Link to='/resume'> <button className='button'>Resume</button></Link>
+            <a href='BayleyArensResume-postNebula-unfinished.pdf' target='_blank'> <button className='button'>Resume</button></a>
             <Link to='/projects'> <button className='button'>Projects</button></Link>
         </div>
     )
