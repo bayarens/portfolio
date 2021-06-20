a62982819f42d8c6a0de11ec8c65c33953ce0c39
bayarens <bay.arens@gmail.com>
2021-06-20 17:49:20 -0400

    rest of projects added

--- src/Components/ProjectView.js
+++ src/Components/ProjectView.js
@@ -6,15 +6,26 @@ class ProjectView extends React.Component{
     state = {
         projects: [
             {
-                title: 'Calculator',
-                deployedURL: 'https://bayarens.github.io/Calculator/',
-                desc: 'This is the first real project I worked on, it is a basic calculator I made from scratch using JavaScript and styled with HTML and CSS.',
+                title: 'Music Playlist App', 
+                deployedURL: 'https://master.d37fvqqdh284tz.amplifyapp.com/',
+                desc: 'This is a full CRUD application I created that allows the user to create, update, and delete items from a playlist. It was made using React, JSX, PSQL and is fully deployed on an Amazon EC2 instance.'
+            },
+            {
+                title: 'Netflix Replica App',
+                deployedURL: 'placeholder',
+                desc: 'This is a Netflix replica React App that I created using an API.',
                 expanded: false
             },
             {
                 title: 'DemonSlayer',
                 deployedURL: 'https://bayarens.github.io/DemonSlayer-Game/',
-                desc: 'this is a game I made in the canvas API in the JavaScript library',
+                desc: 'This is a game I made in the canvas API in the JavaScript library, and was styled with HTML & CSS. It is deployed via GitHub',
+                expanded: false
+            },
+            {
+                title: 'Calculator',
+                deployedURL: 'https://bayarens.github.io/Calculator/',
+                desc: 'This is the first real project I worked on, it is a basic calculator I made from scratch using JavaScript and styled with HTML and CSS.',
                 expanded: false
             }
         ],
