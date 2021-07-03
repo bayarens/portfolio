aa754ecde91485ddb0874311976b71c11a5a0ef5
bayarens <bay.arens@gmail.com>
2021-07-02 20:52:58 -0400

    resume update

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar(props){
     return(
         <div id='navBar'>
             <Link to='/'> <button className='button'>Home</button> </Link>
-            <a href='BayleyArensResume-postNebula-unfinished.pdf' target='_blank'> <button className='button'>Resume</button></a>
+            <a href='BayleyArens-ResumepostNebula.pdf' target='_blank'> <button className='button'>Resume</button></a>
             <Link to='/projects'> <button className='button'>Projects</button></Link>
         </div>
     )
