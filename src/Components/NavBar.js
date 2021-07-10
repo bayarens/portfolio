4028ffb740a28f1ffafa77e548cf93c5037cbc90
bayarens <bay.arens@gmail.com>
2021-07-10 12:17:35 -0400

    resume fix maybe?

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar(props){
     return(
         <div id='navBar'>
             <Link to='/'> <button className='navButton'>Home</button> </Link>
-            <a href='Resume.V3.pdf' target='_blank'> <button className='navButton'>Resume</button></a>
+            <a href='./Resume.V3.pdf' target='_blank'> <button className='navButton'>Resume</button></a>
             <Link to='/projects'> <button className='navButton'>Projects</button></Link>
         </div>
     )
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Resume() {
     return (
         <div id='resume-box'>
-            <embed width='800px' height='500px' src='./Resume.V3.pdf'/>
+            <embed width='800px' height='500px' src='Resume.V3.pdf'/>
         </div>
     )
 }
