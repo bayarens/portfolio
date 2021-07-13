ff70925aa03df080f8a8972052663dfc992e1002
bayarens <bay.arens@gmail.com>
2021-07-13 14:45:41 -0400

    resume fix? part?

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,7 +6,7 @@ export default function NavBar(props){
     return(
         <div id='navBar'>
             <Link to='/'> <button className='navButton'>Home</button> </Link>
-            <a href={resume} target='_blank'> <button className='navButton'>Resume</button></a>
+            <a href='https://bayleyarens.com/Resume.pdf' target='_blank'> <button className='navButton'>Resume</button></a>
             <Link to='/projects'> <button className='navButton'>Projects</button></Link>
         </div>
     )
