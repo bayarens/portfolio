567c589e20b633c596a701e9bf7f688c60d95545
bayarens <bay.arens@gmail.com>
2021-07-13 14:54:16 -0400

    resume fix? part: who knows

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import resume from './Resume.pdf';  
+
 
 export default function NavBar(props){
     return(
         <div id='navBar'>
             <Link to='/'> <button className='navButton'>Home</button> </Link>
-            <a href='https://bayleyarens.com/Resume.pdf' target='_blank'> <button className='navButton'>Resume</button></a>
+            <Link to='/resume'><button className='navButton'>Resume</button> </Link>
+            {/* <a href='https://bayleyarens.com/Resume.pdf' target='_blank'> <button className='navButton'>Resume</button></a> */}
             <Link to='/projects'> <button className='navButton'>Projects</button></Link>
         </div>
     )
